25cf041e66a50f0d2a996e2e66a1f709a5b29bda
nanarama <annasampers@gmail.com>
2023-11-07 12:02:24 -0600

    Add Play page, Header, characterBanner img, react routes

---
+++ app/javascript/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default Header = () => {
+  return (
+    <div className="container-fluid">
+      <img src="charactersBanner.jpg" className="img-fluid"/>
+    </div>
+  )
+}
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
+import Play from "../components/Play";
 
 export default (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/play" element={<Play />} />
     </Routes>
   </Router>
 );
