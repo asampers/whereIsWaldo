519ef46a64b9ef4311e0ddcadbfc05b30d9d7d4a
nanarama <annasampers@gmail.com>
2023-11-08 16:18:22 -0600

    Adjust header spacing

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default Header = () => {
   return (
-    <div className="header container-fluid">
+    <div className="header container">
       <img src="charactersBanner.jpg" className="img-fluid"/>
     </div>
   )
--- app/javascript/components/Play.jsx
+++ app/javascript/components/Play.jsx
@@ -1,14 +1,15 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import GameImage from "./GameImage";
 import Target from "./TargetBox";
 import GuessBtn from "./GuessBtn";
 
 export default Play = () => {
-  //const targetRef = useRef(null);
   const [position, setPosition] = useState({x:0, y: 0, show: false})
-  const characters = [{name: "Da Vinci", x:152, y:635}, {name: "Kahlo", x:655, y:554}, {name: "Picasso", x:588, y:1228}, {name: "Van Gogh", x:135, y:1101}, {name: "Warhol", x:62, y:411}];
   const [foundNames, setFoundNames] = useState([]);
+  
+  const characters = [{name: "Da Vinci", x:152, y:635}, {name: "Kahlo", x:655, y:554}, {name: "Picasso", x:588, y:1228}, {name: "Van Gogh", x:135, y:1101}, {name: "Warhol", x:62, y:411}];
+
   let characterList = characters.map((charac, i) => {
     if(!foundNames.includes(charac.name)) {
       return <GuessBtn key={i} onClick={() => handleTarget(charac)}>{charac.name}</GuessBtn>
@@ -29,8 +30,8 @@ export default Play = () => {
     let right = position.x + 30
     let top = position.y - 40
     let bottom = position.y + 40
-    console.log(`${left} <= ${charac.x} && ${charac.x} <= ${right}`)
-    console.log(`${top} <= ${charac.y} && ${charac.y} <= ${bottom}`)
+    //console.log(`${left} <= ${charac.x} && ${charac.x} <= ${right}`)
+    //console.log(`${top} <= ${charac.y} && ${charac.y} <= ${bottom}`)
     let xBtwn = left <= charac.x && charac.x <= right;
     let yBtwn = top <= charac.y && charac.y <= bottom;
     return xBtwn && yBtwn;
