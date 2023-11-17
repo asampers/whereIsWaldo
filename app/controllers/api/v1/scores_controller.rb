b8b6d19d9a07e54dc2bcf272db5b25abd4bb0341
nanarama <annasampers@gmail.com>
2023-11-17 16:29:46 -0600

    Update Score controller methods

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -1,7 +1,21 @@
 class Api::V1::ScoresController < ApplicationController
   def index
+    score = Score.all.order(time: :asc)
+    render json: score
   end
 
   def create
+    score = Score.create!(score_params)
+    if score
+      render json: score
+    else  
+      render json: score.errors
+    end    
+  end
+
+  private
+
+  def score_params
+    params.require(:name, :time)
   end
 end
