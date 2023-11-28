8af044f4982005dc2c67f5c3a5b7357f6db4965f
nanarama <annasampers@gmail.com>
2023-11-28 17:14:19 -0600

    Add character request tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require_relative 'support/factory_bot'
-require_relative 'support/chrome'
-
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require_relative 'support/factory_bot'
+require_relative 'support/chrome'
+require_relative 'support/request_helpers'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -35,7 +34,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -63,6 +62,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
 end
 
 Shoulda::Matchers.configure do |config|
--- spec/requests/api/v1/characters_spec.rb
+++ spec/requests/api/v1/characters_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Characters", type: :request do
   describe "GET /index" do
+    before do
+      FactoryBot.create_list(:character, 5)
+      get "/api/v1/characters/index" 
+    end
+    
+    it "returns all characters" do 
+      
+      expect(json.count).to eq(5)
+    end
+
     it "returns http success" do
-      get "/api/v1/characters/index"
       expect(response).to have_http_status(:success)
     end
   end
